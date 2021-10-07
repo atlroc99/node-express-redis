74d018c3627d1f3d96a55070d1aa4142bb742723
jeffry zaman <jeffry.zaman@gmail.com>
2021-10-07 03:07:18 -0400

    updated put and post method

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-
 const express = require('express');
 const redis = require('redis')
 const bodyParser = require('body-parser');
 const shortid = require('shortid')
+const cors = require('cors')
 
 const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const client = redis.createClient()
 client.on('err', (err) => {
@@ -21,34 +22,34 @@ app.get('/:id', async (req, res) => {
     client.get(id, (err, object) => {
         if (err) throw err;
         console.log('BEFORE PARSE OBJECT : ', object)
-        person = JSON.parse(object)
-        console.log('AFTER PARSE OBJECT : ', person)
-        const _perosn = JSON.stringify(person)
-        res.status(200).json(person)
+        res.status(200).json(object)
+        // parse_object = JSON.parse(object)
+        // console.log('AFTER PARSE OBJECT : ', parse_object)
+        // const stringify_object = JSON.stringify(parse_object)
     })
 })
 
-app.put('/:id', async (req, res) => {
-    const id = req.params.id;
-    const { firstName, lastName } = req.body
-    const person = { firstName, lastName }
-    client.set(id, JSON.stringify(person))
-    res.status(200).end();
+app.put('/:id', (req, res) => {
+    console.log('updating data | customerID:', req.params.id);
+    const data = req.body
+    const id = req.params.id === 'NIL' ? shortid.generate() : req.params.id;
+    client.set(id, JSON.stringify(data))
+    res.status(201).json({ 'id': id }).end();
 });
 
-app.post('/', async (req, res) => {
-    const { firstName, lastName } = req.body;
-    const person = { firstName, lastName }
-    id = shortid.generate()
-    client.set(id, JSON.stringify(person));
-    res.status(201).json({"id": id});
+app.post('/:id', (req, res) => {
+    const data = req.body;
+    const id = req.params.id
+    console.log(`ID: ${id} & GOT BODY!! ${data}`,)
+    client.set(id, JSON.stringify(data));
+    res.status(201).json({ "id": id }).end();
 })
 
 app.delete('/:id', async (req, res) => {
     id = req.params.id;
     console.log(`Delete object for ID:', ${id}`);
     client.del(id);
-    res.status(204).json({'msg': `delete ${id}`}).end();
+    res.status(204).json({ 'msg': `delete ${id}` }).end();
 })
 
 app.listen(8000, () => {
