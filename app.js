740bcf519ec6bab58cec6e5071559a355bfc7e0f
jeffry zaman <jeffry.zaman@gmail.com>
2021-10-07 05:13:53 -0400

    added send email functionality

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 const express = require('express');
-const redis = require('redis')
+const redis = require('redis');
 const bodyParser = require('body-parser');
-const shortid = require('shortid')
+const shortid = require('shortid');
+const nodemailer = require('nodemailer');
 const cors = require('cors')
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const client = redis.createClient()
+const client = redis.createClient();
 client.on('err', (err) => {
     console.log(err);
 })
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "autobots.test.user@gmail.com",
+        pass: process.env.PASS
+    }
+});
 
 
 app.get('/:id', async (req, res) => {
     const id = req.params.id
     console.log('ID:', id);
-
     client.get(id, (err, object) => {
         if (err) throw err;
         console.log('BEFORE PARSE OBJECT : ', object)
         res.status(200).json(object)
-        // parse_object = JSON.parse(object)
-        // console.log('AFTER PARSE OBJECT : ', parse_object)
-        // const stringify_object = JSON.stringify(parse_object)
     })
 })
 
@@ -52,8 +57,26 @@ app.delete('/:id', async (req, res) => {
     res.status(204).json({ 'msg': `delete ${id}` }).end();
 })
 
+app.post('/welcome/sendmail', async (req, res) => {
+    console.log('env var:', process.env.PASS)
+    data = req.body.message
+    console.log('req.body', data)
+    const mailOptions = {
+        from: 'autobots.test.user@gmail.com',
+        to: 'mohammad.zaman@carrier.com',
+        subject: 'Welcome package updated notification',
+        text: data
+    }
+
+transporter.sendMail(mailOptions, (error, response) => {
+        if (error) throw error;
+        console.log(response);
+        res.send(response);
+        res.redirect('/');
+    })
+})
+
 app.listen(8000, () => {
     console.log('NODE SERVER STARTED')
+    console.log('PASS: ', process.env.PASS)
 });
-
-
