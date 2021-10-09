1a451261d890fe4da074d9d3f4216d97649eee45
jeffry zaman <jeffry.zaman@gmail.com>
2021-10-09 04:42:47 -0400

    need to check new data structure

--- checklist.js
+++ checklist.js
@@ -1,20 +1,45 @@
-const checklist = [{
-    id: 1,
-    name: 'iTSecurityDocReviewedAndApproved',
-    isChecked: false,
-    label: 'IT security documents on BACnet/SC & i-Vu® Cloud reviewed and approved'
-},
-{
-    id: 2,
-    name: 'outboundPortEnabled',
-    isChecked: false,
-    label: 'Outbound Port availability (port 443 as default) enabled. (Following the IT Security documents listed above, BACnet/SC requires outbound communication to the cloud hosted BACnet/SC hub. If IT requires the i-Vu® Service URL to be whitelisted, note this on the provisioning form when ordering the license.)'
-},
-{
-    id: 3,
-    name: 'certAuthIdentified',
-    isChecked: false,
-    label: 'Certificate authority identified which will be used to generate BACnet/SC certificates'
-}]
+const checklist = [
+    {
+        id: 1,
+        name: 'iTSecurityDocReviewedAndApproved',
+        isChecked: false,
+        label: 'IT security documents on BACnet/SC & i-Vu® Cloud reviewed and approved'
+    },
+    {
+        id: 2,
+        name: 'outboundPortEnabled',
+        isChecked: false,
+        label: 'Outbound Port availability (port 443 as default) enabled. (Following the IT Security documents listed above, BACnet/SC requires outbound communication to the cloud hosted BACnet/SC hub. If IT requires the i-Vu® Service URL to be whitelisted, note this on the provisioning form when ordering the license.)'
+    },
+    {
+        id: 3,
+        name: 'certAuthIdentified',
+        isChecked: false,
+        label: 'Certificate authority identified which will be used to generate BACnet/SC certificates'
+    }]
 
 module.exports = checklist
+
+// const checklist = {
+//     isSubmitted: false,
+//     items: [
+//         {
+//             id: 1,
+//             name: 'iTSecurityDocReviewedAndApproved',
+//             isChecked: false,
+//             label: 'IT security documents on BACnet/SC & i-Vu® Cloud reviewed and approved'
+//         },
+//         {
+//             id: 2,
+//             name: 'outboundPortEnabled',
+//             isChecked: false,
+//             label: 'Outbound Port availability (port 443 as default) enabled. (Following the IT Security documents listed above, BACnet/SC requires outbound communication to the cloud hosted BACnet/SC hub. If IT requires the i-Vu® Service URL to be whitelisted, note this on the provisioning form when ordering the license.)'
+//         },
+//         {
+//             id: 3,
+//             name: 'certAuthIdentified',
+//             isChecked: false,
+//             label: 'Certificate authority identified which will be used to generate BACnet/SC certificates'
+//         }]
+// }
+// module.exports = checklist
